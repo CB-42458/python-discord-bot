d24317d2de718cd5652c441142f519ff4287a152
stevebrown4589gmail <cb50243987@gmail.com>
2023-05-12 12:34:18 +0100

    Update bot.py
    Added some comments to the file

--- bot/bot.py
+++ bot/bot.py
@@ -2,14 +2,16 @@
 This program run the bot and loads the cogs
 """
 
-import datetime as dt
-import os
-from platform import python_version
+import datetime as dt  # used for the time and date in the logs
+from platform import python_version  # used for getting the python version for the logs
+import os  # used for getting the path to the .env file
 
+from dotenv import load_dotenv  # used for loading the .env file
+# imports used for the discord bot
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 
+# loads the .env file and creates global variables for the server and the owner
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD_ID = os.getenv("GUILD_ID")
@@ -21,18 +23,19 @@ class Client(commands.Bot):
     """
     This class is the bot
     """
-    def __init__(self):
+
+    def __init__(self) -> None:
         super().__init__(command_prefix=commands.when_mentioned_or('.'), intents=discord.Intents.all())
 
     async def setup_hook(self) -> None:
         """
         Loads the cogs
         """
-        cogs = []
+        cogs = ("cogs.generalCommandsCog")
         for cog in cogs:
             await self.load_extension(cog)
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         """
         Prints the time, the event, and the bot's name and that it is online
         """
@@ -51,11 +54,14 @@ class Client(commands.Bot):
               f'\033[0;35mdiscord.client.event | on_ready\033[0;31m Python Version: {python_version()}\033[0;0m',
               flush=True)
 
+        # syncs the commands to the server
         synced = await self.tree.sync(guild=GUILD)
 
+        # prints the time, the event, and the number of commands synced
         print(f'\033[1;30m{dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\033[1;34m INFO     ' +
-              f'\033[0;35mdiscord.client.event | on_ready\033[0;31m Synced {synced} commands\033[0;0m', flush=True)
+              f'\033[0;35mdiscord.client.event | on_ready\033[0;31m Synced {len(synced)} commands\033[0;0m', flush=True)
 
 
+# runs the bot
 bot = Client()
 bot.run(DISCORD_TOKEN)
