7ef27d38b84cc63d3b741d0c84b60dd49242d76b
stevebrown4589gmail <cb50243987@gmail.com>
2023-05-23 12:18:47 +0100

    Update bot.py
    Changed the print statements to use the logger package.
    I also added a function which would automatically create a array of strings containing the syntax that the load_extension method likes for all the files in the cog folder which end in "Cog.py". So if it is not actually a cog then an error will be thrown.

--- bot/bot.py
+++ bot/bot.py
@@ -2,14 +2,13 @@
 This program run the bot and loads the cogs
 """
 
-import datetime as dt  # used for the time and date in the logs
-from platform import python_version  # used for getting the python version for the logs
 import os  # used for getting the path to the .env file
+from platform import python_version  # used for getting the python version for the logs
 
-from dotenv import load_dotenv  # used for loading the .env file
-# imports used for the discord bot
 import discord
+import logger
 from discord.ext import commands
+from dotenv import load_dotenv  # used for loading the .env file
 
 # loads the .env file and creates global variables for the server and the owner
 load_dotenv()
@@ -17,6 +16,32 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD_ID = os.getenv("GUILD_ID")
 GUILD = discord.Object(GUILD_ID)
 OWNER_ID = os.getenv("OWNER_ID")
+
+
+def find_cogs(path: str | None = None, cogs_path: str | None = None) -> list[str]:
+    """
+    Finds the cogs in the cogs folder, it will add the path to the cogs to the list cogs if it ends with "Cog.py"
+    """
+    # creates an empty list
+    cog_list: list[str] = []
+    # if the path is None then it is set to the ./cogs folder
+    path = path or os.path.join(os.path.dirname(os.path.abspath(__file__)), "cogs")
+    # the variable cog_path is used for the method load_extension in the setup_hook method of the Client class and the
+    # method likes to have the paths to the cogs in a specific format e.g. "cogs.generalCommandsCog" hence the variable
+    # cogs_path is used to store the path to the cogs in that format
+    cogs_path = cogs_path or "cogs."
+    # for each file in the path
+    for file in os.listdir(path):
+        # if the file is a folder
+        if os.path.isdir(os.path.join(path, file)):
+            # find the cogs in the folder
+            cog_list += find_cogs(os.path.join(path, file), cogs_path + file + ".")
+        # if the file ends with "Cog.py"
+        elif file.endswith("Cog.py"):
+            # add the path to the cogs to the list cogs
+            cog_list.append(cogs_path + file[:-3])
+
+    return cog_list
 
 
 class Client(commands.Bot):
@@ -30,36 +55,25 @@ class Client(commands.Bot):
     async def setup_hook(self) -> None:
         """
         Loads the cogs
-        """
-        cogs = ["cogs.generalCommandsCog"]
+        """    
+        cogs: list[str] = []
+        cogs += find_cogs()
         for cog in cogs:
             await self.load_extension(cog)
 
     async def on_ready(self) -> None:
         """
-        Prints the time, the event, and the bot's name and that it is online
+        This method is called when the bot is ready
         """
-        # prints the time, the event, and the bot's name and that it is online
-        print(f'\033[1;30m{dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\033[1;34m INFO     ' +
-              f'\033[0;35mdiscord.client.event | on_ready\033[0;31m {self.user} is online\033[0;0m', flush=True)
-        # prints the time, the event, and the bot's ID
-        print(f'\033[1;30m{dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\033[1;34m INFO     ' +
-              f'\033[0;35mdiscord.client.event | on_ready\033[0;31m Bot ID: {self.user.id}\033[0;0m', flush=True)
-        # prints the time, the event, and the discord version
-        print(f'\033[1;30m{dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\033[1;34m INFO     ' +
-              f'\033[0;35mdiscord.client.event | on_ready\033[0;31m Discord Version: {discord.__version__}\033[0;0m',
-              flush=True)
-        # prints the time, the event, and the python version
-        print(f'\033[1;30m{dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\033[1;34m INFO     ' +
-              f'\033[0;35mdiscord.client.event | on_ready\033[0;31m Python Version: {python_version()}\033[0;0m',
-              flush=True)
+        # logs the bots details
+        logger.log("INFO", "discord.on_ready", f"{self.user} is online")
+        logger.log("INFO", "discord.on_ready", f"Bot ID: {self.user.id}")
+        logger.log("INFO", "discord.on_ready", f"Discord Version: {discord.__version__}")
+        logger.log("INFO", "discord.on_ready", f"Python Version: {python_version()}")
 
-        # syncs the commands to the server
+        # syncs the commands to the server and logs how many commands were synced
         synced = await self.tree.sync(guild=GUILD)
-
-        # prints the time, the event, and the number of commands synced
-        print(f'\033[1;30m{dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\033[1;34m INFO     ' +
-              f'\033[0;35mdiscord.client.event | on_ready\033[0;31m Synced {len(synced)} commands\033[0;0m', flush=True)
+        logger.log("INFO", "discord.on_ready", f"Synced {len(synced)} commands")
 
 
 # runs the bot
